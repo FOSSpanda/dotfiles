1407840ccfcdc5ca35c8255cc8c3cbb37c234489
FOSSpanda <chris@suddenmoves.uk>
2024-06-07 22:02:40 +0100

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,4 +1,4 @@
 if status is-interactive
-    oh-my-posh init fish | source
+    oh-my-posh init fish --config ~/.config/ohmyposh/tdp.toml    | source
     # Commands to run in interactive sessions can go here
 end
