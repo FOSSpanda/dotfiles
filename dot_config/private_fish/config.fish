7267fd6b2d359268257db8c5480bbba39c6425ed
FOSSpanda <chris@suddenmoves.uk>
2024-06-10 23:07:13 +0100

    Update .config/fish/config.fish Update .config/sway/config Update .config/waybar/config Update .config/wezterm/wezterm.lua Add .local/bin/setup.sh

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,4 +1,5 @@
 if status is-interactive
+    fish_add_path -g -p ~/.local/bin ~/.cargo/bin
     hyfetch
     oh-my-posh init fish --config ~/.config/ohmyposh/tdp.toml | source
     abbr --add ls --position command "eza --icons"
---
+++ private_dot_local/bin/executable_setup.sh
@@ -0,0 +1,61 @@
+echo "package install"
+read -p "Press key to continue.. " -n1 -s
+
+if [ -f "/etc/arch-release" ]; then
+  sudo pacman -Syu
+  sudo pacman -Sy --needed \
+    stow sway swaylock mako bat most less micro \
+    git fuzzel fzf thunar curl hyfetch flatpak pkg-config \
+    xorg-xwayland firefox xdg-desktop-portal-wlr \
+    xdg-desktop-portal-gtk noto-fonts noto-fonts-emoji \
+    waybar pavucontrol cmake linux-headers gvfs polkit-gnome \
+    thunar-archive-plugin thunar-volman unzip meson \
+    xdg-user-dirs jq grim slurp sway-contrib wl-clipboard \
+    discord telegram-desktop
+
+elif [ -f "/etc/debian-version"]; then
+  sudo dpkg --add-architecture i386
+
+  sudo apt update
+
+  sudo apt install \
+    stow sway swayidle swaylock mako-notifier bat most less micro \ 
+    git fuzzel fzf thunar curl hyfetch flatpak pkg-config \
+    xdg-desktop-portal-wlr xdg-desktop-portal-gtk fonts-font-awesome  \
+    xwayland firefox fonts-noto fonts-noto-color-emoji \
+    waybar breeze-icon-theme nala libfontconfig-dev \
+    pavucontrol cmake linux-headers-amd64 gvfs polkit-kde-agent-1 \
+    thunar-archive-plugin thunar-volman thunar-gtkhash unzip \
+    cmake meson xdg-user-dirs jq pamixer grim slurp \
+    steam-installer steam-devices autotiling libfuse2 \
+    wl-clipboard
+
+  wget https://discord.com/api/download?platform=linux&format=deb -O ~/Downloads/discord.deb
+
+  sudo apt install ./Downloads/discord.deb
+
+else
+  exit "Only setyup for debian and arch!" 1;
+fi
+
+echo "flatpak time"
+read -p "Press key to continue.. " -n1 -s
+
+flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+if [ -f "/etc/debian-version"]; then
+  flatpak install org.telegram.desktop
+fi
+
+echo "rust"
+read -p "Press key to continue.. " -n1 -s
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+. .cargo/env
+
+cargo install alacritty
+cargo install eza
+cargo install zoxide
+cargo install vivid
+
+exit
