578243c72d4b3b0aa5ba4c68f5720ba0b37440f9
the Dev Panda <mail@devpanda.uk>
2024-06-18 14:16:41 +0100

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     fish_add_path -g -p ~/.local/bin ~/.cargo/bin
     hyfetch
     oh-my-posh init fish --config ~/.config/ohmyposh/tdp.toml | source
-    abbr --add ls --position command "eza --icons"
+    abbr --add ls eza --icons
     set -gx EDITOR helix
     # Commands to run in interactive sessions can go here
 end
