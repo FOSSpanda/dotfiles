f383d70737ed0ace7387911c63de27e3dd9333d0
the Dev Panda <mail@devpanda.uk>
2024-07-06 10:25:18 +0100

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,6 +3,6 @@ if status is-interactive
     hyfetch
     oh-my-posh init fish --config ~/.config/ohmyposh/tdp.toml | source
     abbr --add ls eza --icons
-    set -gx EDITOR neovim
+    set -gx EDITOR nvim
     # Commands to run in interactive sessions can go here
 end
