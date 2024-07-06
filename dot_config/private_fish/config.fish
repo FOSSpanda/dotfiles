b146b32dd6b770fa2651670afc7e32f532d10171
the Dev Panda <mail@devpanda.uk>
2024-07-06 10:42:57 +0100

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,11 +1,20 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    fish_add_path -g -p ~/.local/bin ~/.cargo/bin
-    hyfetch
-    oh-my-posh init fish --config ~/.config/ohmyposh/tdp.toml | source
+# Abbreviations
     abbr --add ls eza --icons
     abbr --add cze chezmoi edit
     abbr --add cza chezmoi apply
+    abbr --add bat batcat
+    abbr --add cat batcat
+    abbr --add less most
+    abbr --add cd.. "cd .."
+
+# Set some global variables
     set -gx EDITOR micro
     set -g fish_greeting
+
+# Commands to run in interactive sessions can go here
+    fish_add_path -g -p ~/.local/bin ~/.cargo/bin
+    hyfetch
+    oh-my-posh init fish --config ~/.config/ohmyposh/tdp.toml | source
+
 end
