292abb15e4d10b7fe4d46326a6c930b3a965681e
the Dev Panda <mail@devpanda.uk>
2024-07-06 10:24:00 +0100

    Update .local/bin/setup.sh

--- private_dot_local/bin/executable_setup.sh
+++ private_dot_local/bin/executable_setup.sh
@@ -20,7 +20,8 @@ fi
     thunar-archive-plugin thunar-volman thunar-gtkhash unzip \
     cmake meson xdg-user-dirs jq pamixer grim slurp \
     steam-installer steam-devices autotiling libfuse2 \
-    wl-clipboard wget mutt-wizard neomutt bpytop
+    wl-clipboard wget mutt-wizard neomutt bpytop \
+    neovim python3-neovim
 
 #Discord
   wget "https://discord.com/api/download?platform=linux&format=deb" -O ~/Downloads/discord.deb
