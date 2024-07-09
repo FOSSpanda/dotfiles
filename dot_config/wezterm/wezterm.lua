bd5ab907865448f4f1a443c063a6b24eeab7fd40
the Dev Panda <mail@devpanda.uk>
2024-07-09 04:02:43 +0100

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -8,7 +8,7 @@ config.font = wezterm.font 'Comic Code'
 config.font_size = 14
 config.color_scheme = "Catppuccin Frappe"
 config.use_fancy_tab_bar = false
-config.window_background_opacity = 0.5
+config.window_background_opacity = 0.8
 
 -- and finally, return the configuration to wezterm
 return config
