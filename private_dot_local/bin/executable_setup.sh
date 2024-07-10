6c100f81fb692eddec72ca816edef88efeb319e3
the Dev Panda <mail@devpanda.uk>
2024-07-10 17:26:53 +0100

    Update .local/bin/setup.sh

--- private_dot_local/bin/executable_setup.sh
+++ private_dot_local/bin/executable_setup.sh
@@ -51,6 +51,11 @@ if [  -f "/etc/debian_version" ]; then
   sudo meson install -C build
   cd ~
 
+#Spotify
+curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
+
 else
   exit "Only setup for debian!" 1;
 fi
