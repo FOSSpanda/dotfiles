9888807a555b4aef393a127f083b714f4d3f0568
the Dev Panda <mail@devpanda.uk>
2024-07-11 21:55:34 +0100

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -10,7 +10,7 @@ if status is-interactive
     abbr --add cd.. "cd .."
 
 # Set some global variables
-    set -gx EDITOR nvim
+    set -gx EDITOR micro 
     set -g fish_greeting
     set -gx MICRO_TRUECOLOR 1
 # Commands to run in interactive sessions can go here
